0e371020ed0239e147c1d89bed5b5692bde486f3
Garrett Dimon <email@garrettdimon.com>
2021-04-17 11:04:16 -0600

    Update reviewer test.

--- test/reviewer_test.rb
+++ test/reviewer_test.rb
@@ -2,12 +2,10 @@
 
 require "test_helper"
 
-class ReviewerTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Reviewer::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
+module Reviewer
+  class ReviewerTest < Minitest::Test
+    def test_that_it_has_a_version_number
+      refute_nil VERSION
+    end
   end
 end
