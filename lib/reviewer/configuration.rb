5751b2886eccc4d40b46a037ad32ea51af58a6f0
Garrett Dimon <email@garrettdimon.com>
2021-04-17 13:41:56 -0600

    Making default configuration file location more explicit.

--- lib/reviewer/configuration.rb
+++ lib/reviewer/configuration.rb
@@ -2,10 +2,13 @@
 
 module Reviewer
   class Configuration
+    DEFAULT_CONFIGURATION_PATH = Dir.pwd.freeze
+    DEFAULT_CONFIGURATION_FILE = '.reviewer.yml'.freeze
+
     attr_accessor :file
 
     def initialize
-      @file = 'commands.yml'
+      @file = "#{DEFAULT_CONFIGURATION_PATH}/#{DEFAULT_CONFIGURATION_FILE}"
     end
   end
 end
