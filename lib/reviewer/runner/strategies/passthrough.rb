2e0dbb6e184bc5abfd55acd09f65d499f3980282
Garrett Dimon <email@garrettdimon.com>
2021-10-19 17:35:12 -0600

    Fill in some documentation details.

--- lib/reviewer/runner/strategies/passthrough.rb
+++ lib/reviewer/runner/strategies/passthrough.rb
@@ -3,7 +3,9 @@
 module Reviewer
   class Runner
     module Strategies
-      # Execution strategy for a runner to run a command transparently
+      # Execution strategy for a runner to run a command transparently—displaying the command's
+      #   output in realtime as it runs (as opposed to capturing and suppressing the output unless
+      #   the command fails)
       class Passthrough
         attr_accessor :runner
 
